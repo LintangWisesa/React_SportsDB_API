2f4e2b6c9c9b2276e8233924776f818b00441e8a
lintangwisesa <lintangwisesa@ymail.com>
2018-12-05 11:27:51 +0700

    react laliga

---
+++ src/komponen/Kontak.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class Kontak extends Component {
+    render(){
+        return(
+            <div>
+                <h4>Klubola.kom</h4>
+                <h4>Jl. Stadion Utama 123</h4>
+                <h4>(021)123456</h4>
+            </div>
+        )
+    }
+}
+
+export default Kontak

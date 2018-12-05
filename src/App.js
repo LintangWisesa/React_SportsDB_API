4ab51f3067f23a31a1b4f4685d8bd03175103fba
lintangwisesa <lintangwisesa@ymail.com>
2018-12-05 11:26:39 +0700

    react laliga

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+import Kontak from './komponen/Kontak'
+import Beranda from './komponen/Beranda'
+import ProfilKlub from './komponen/ProfilKlub'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+  render(){
+    return(
+      <div>
+        <h1>Klubola.kom</h1>
+        <div>
+          <Route exact path='/' component={Beranda}/>
+          <Route path='/kontak' component={Kontak}/>
+          <Route path='/:profil' component={ProfilKlub}/>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
